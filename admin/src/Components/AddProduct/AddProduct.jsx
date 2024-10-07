72ed444464008ac2e741d0fc9050d4b787a8f7eb
Rahil RiyazAhmed Saiyed <102587430+rahilsaiyed1711@users.noreply.github.com>
2024-10-07 13:04:15 +0530

    Update AddProduct.jsx

--- admin/src/Components/AddProduct/AddProduct.jsx
+++ admin/src/Components/AddProduct/AddProduct.jsx
@@ -24,7 +24,7 @@ const AddProduct = () => {
         let product = productDetails;
         let formData = new FormData();
         formData.append('product', image);
-        await fetch('http://localhost:4000/upload', {
+        await fetch('https://ecommerce-backend-38z9.onrender.com/upload', {
             method: 'POST',
             headers: {
                 Accept: 'application/json',
@@ -34,7 +34,7 @@ const AddProduct = () => {
         if (responseData.success) {
             product.image = responseData.image_url;
             console.log(product)
-            await fetch('http://localhost:4000/addproduct', {
+            await fetch('https://ecommerce-backend-38z9.onrender.com/addproduct', {
                 method: 'POST',
                 headers: {
                     Accept: 'application/json',
